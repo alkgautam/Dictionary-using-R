537d7954ad5c605de67ac1a5c61de8e861ccc742
alkgautam <alkgautam@gmail.com>
2017-09-02 14:23:13 +0530

    Update dict.R

--- dict.R
+++ dict.R
@@ -1,23 +1,33 @@
+# This Dictionary is the work done by Alok Gautam
+# The txt file included in could be found at Project Gutenberg.
+# No extra R Packages are needed except the base.
+
 # creating a connection to read the lines from the text
 
-con <- file("./files/dict.txt")
+con <- file("../files/dict.txt")
 dict <- readLines(con = con)
 close(con = con)
 
 # subsetting the words
 dictfinal <- dict[28:973899]
+check <- 1
 
 # Extracting the words from the text using Regular Expressions
 wordlist <- grep(pattern = "^[A-Z]$|^[A-Z][A-Z]$|^[A-Z][A-Z][A-Z]",x = dictfinal)
 words <- dictfinal[wordlist]
 
+while(check == 1) {
 # Inputting the word whose meaning is to be found
 userip <- readline(prompt = "Enter the word : ")
 print(userip)
 
 # changing the value to upper case
 word <- toupper(userip)
+
+# Merging the Dictionary words with their Row Numbers
 all <- data.frame(wordlist,words,stringsAsFactors = F)
+
+# Finding the row numbers of the matched users' word
 match <- grep(pattern = word,x = all[,2])
 df1 <- as.data.frame(all[match,2])
 colnames(df1) <- c("matched")
@@ -28,7 +38,9 @@ if(length(match) == 1) {
         rownum <- as.numeric(rownames(vec[vec$pos %in% 1,]))
         pos1 <- all[rownum,1]
         pos2 <- all[rownum + 1, 1]
+        cat(" ___________________________________________________________________________________ \n")
         print(dictfinal[pos1 : (pos2 - 2)])
+        cat(" ___________________________________________________________________________________ \n")
 } else if (length(match) == 0)
 { print("The word entered is not in the Dictionary")
 } else {
@@ -40,4 +52,9 @@ if(length(match) == 1) {
         cat(" ___________________________________________________________________________________ \n")
         print(dictfinal[pos1 : (pos2 - 2)])
         cat(" ___________________________________________________________________________________ \n")
+}
+        cat("Do you Wish to search more words? \n Enter : \n 1. Yes \n 2. No \n")
+        ip <- readline()
+        ip <- as.numeric(ip)
+        if (ip <- 2) {break}
 }
